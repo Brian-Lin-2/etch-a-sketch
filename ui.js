b96bd826eb1746da96fdf0505b78eb4db0b44b9a
Brian <linbrian24@gmail.com>
2023-03-15 18:29:59 -0400

    Make createGrids() accept paramters

--- ui.js
+++ ui.js
@@ -1,10 +1,9 @@
-const createGrids = () => {
-    // Make 16 columns of 16 divs.
-    for (let col = 0; col < 16; col++) {
+const createGrids = (num) => {
+    for (let col = 0; col < num; col++) {
         const colGrid = document.createElement('div');
         colGrid.classList.add(`grid__row`);
 
-        for (let row = 0; row < 16; row++) {
+        for (let row = 0; row < num; row++) {
             const rowGrid = document.createElement('div');
             rowGrid.classList.add('grid__square')
 
@@ -15,10 +14,10 @@ const createGrids = () => {
     }
 }
 
-// Create a 16 x 16 grid using divs.
 const grid = document.querySelector('.grid');
 
-createGrids();
+// 16 is default.
+createGrids(16);
 
 // Creates hover effect.
 const gridSquare = document.querySelectorAll('.grid__square');
